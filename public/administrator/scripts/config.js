9053a2d1d543da541c28d323ae319276826a2483
budsalvetti <budsalvetti@gmail.com>
2016-11-06 18:39:52 -0800

    Create config.js

---
+++ public/administrator/scripts/config.js
@@ -0,0 +1,37 @@
+/* global requirejs, define*/
+
+'use strict'
+
+requirejs.onError = function(error){
+    
+    console.log(error);
+    throw error;
+};
+
+require.config({
+    enforceDefine: false,
+    xhtml:false,
+    waitseconds:15,
+    config:{
+        text:{
+            env:'xhr'
+        }
+    },
+    paths:{
+        'bower_components': '../bower_components',
+        'config': './config',
+        'app': './app',
+        'angular': '../bower_components/angular/angular',
+        'angular-ui-router': '../bower_components/angular-ui-router/release/angular-ui-router',
+        'jquery': '../bower_components/jquery/dist/jquery.min'
+    },
+    shim:{
+        'angular':{
+            deps: ['jquery'],
+            exports: 'angular'
+        },
+        'angular-ui-router':{
+            deps:['angular']
+        },
+        'app':['angular']
+    }});
