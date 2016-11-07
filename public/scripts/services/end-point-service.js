93f589c7f0b3eceec732f240af4ad2e5bb3203d6
budsalvetti <budsalvetti@gmail.com>
2016-11-06 19:55:20 -0800

    Create end-point-service.js

---
+++ public/scripts/services/end-point-service.js
@@ -0,0 +1,6 @@
+define(['angular'],function(angular){
+    return angular.module('services.EndPointService',[]).factory('EndPointService',function(){
+        
+        return {ipAddress:'54.153.42.53:8080'};
+    });
+});
