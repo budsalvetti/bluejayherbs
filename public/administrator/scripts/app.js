b6487b1a0d094a32358dad941852015e22250846
budsalvetti <budsalvetti@gmail.com>
2016-11-06 18:38:05 -0800

    Create app.js

---
+++ public/administrator/scripts/app.js
@@ -0,0 +1,37 @@
+define(['jquery','angular','routes','./modules/main'],function($,angular){
+    'use strict';
+    
+    return angular.module('adminApp',['App.Routes','EditProducts','services.EndPointService']).controller('MainCtrl',['$rootScope',
+                        '$scope',
+                        '$state',
+                        '$stateParams',
+                        'EndPointService',
+                        function($rootScope,$scope,$state,$stateParams,endPointService){
+                        
+        
+                         window.App = $rootScope.App = {
+                            name: 'adminApp'
+                         };
+        
+                        //set the ip address for any rest operation
+                        $rootScope.ipAddress = endPointService.ipAddress;
+        
+        
+                         $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
+                             if (angular.isObject(error) && angular.isString(error.code)) {
+                                switch (error.code) {
+                                case 'UNAUTHORIZED':
+                                $state.go('login');
+                                }
+                            }
+                            else {
+                                     // unexpected error
+                                }
+                        });
+
+        
+                        alert("in Main controller");
+        
+    }]);
+    
+});
