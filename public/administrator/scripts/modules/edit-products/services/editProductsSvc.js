8c5d9b737679ceb3361c04dc3084a93b665edbd8
Salvetti Eugene <eugene.m.salvetti@ge.com>
2017-08-15 15:17:54 -0700

    whatever

--- public/administrator/scripts/modules/edit-products/services/editProductsSvc.js
+++ public/administrator/scripts/modules/edit-products/services/editProductsSvc.js
@@ -1,32 +1,120 @@
 define(['angular','../module'],function(angular,editProductsModule){
     
     editProductsModule.factory('EditProductsSvc',['$rootScope','$http','$q',function($rootScope,$http,$q){
-        
-        
-        var getAllProducts = function(){
+
+
+			/**
+			 * @function _getAllProducts
+       * @returns {*}
+       * @private
+       */
+      var _getAllProducts = function(){
             
-            console.log("in getAllProducts");
+            console.log("admin in getAllProducts");
             
             var myDeferred = $q.defer();
             
-            $http.get('/admin/getAllProducts').then(function(productData){
-                myDeferred.resolve(productData);
+            $http.get('/admin/getAllProducts').then(function(response){
+                myDeferred.resolve(response);
             })
-            
-            
+
             return myDeferred.promise;
-            
         };
-        
-        
-        var deleteProduct = function(id){
+
+
+			/**
+       * @function _getAllProductCategories
+       * Gets all Product Categories
+       * @returns {*}
+       */
+      var _getAllProductCategories = function(){
+            console.log("admin in getAllProductCategories");
+
+            var myDeferred = $q.defer();
+
+            $http.get('/admin/getAllProductCategories').then(function(response){
+                myDeferred.resolve(response);
+            })
+
+            return myDeferred.promise;
+        };
+
+
+        /**
+         * @function _getAllSizePrices
+         * Gets all size/price objects for a product category
+         * @returns {*}
+         */
+        var _getAllSizePrices = function(productCategoryId){
+            console.log("admin in getAllSizePrices");
+
+            var myDeferred = $q.defer();
+
+            $http.get('/admin/getAllProductCategories?' + productCategoryId).then(function(response){
+                myDeferred.resolve(response);
+            })
+
+            return myDeferred.promise;
+        };
+
+
+        /**
+         * @function _getAllHealthCategories
+         * @returns {*}
+         * @private
+         */
+        var _getAllHealthCategories = function(){
+
+            console.log("admin in getAllHealthCategories");
+
+            var myDeferred = $q.defer();
+
+            $http.get('/admin/getAllHealthCategories').then(function(response){
+                myDeferred.resolve(response);
+            })
+
+            return myDeferred.promise;
+        };
+
+
+        /**
+         * @function _getAllSymptoms
+         * @returns {*}
+         * @private
+         */
+        var _getAllSymptoms = function(){
+
+            console.log("admin in getAllSymptoms");
+
+            var myDeferred = $q.defer();
+
+            $http.get('/admin/getAllSymptoms').then(function(response){
+                myDeferred.resolve(response);
+            })
+
+            return myDeferred.promise;
+        };
+
+
+
+
+        /**
+         * @function _getAllSymptoms
+         * @returns {*}
+         * @private
+         */
+        var _deleteProduct = function(productId){
             console.log("in delete product");
         };
         
         
         return{
-            getAllProducts:getAllProducts,
-            deleteProduct:deleteProduct
+            getAllProducts:_getAllProducts,
+            getAllProductCategories:_getAllProductCategories,
+            getAllSizePrices:_getAllSizePrices,
+            getAllHealthCategories:_getAllHealthCategories,
+            getAllSymptoms:_getAllSymptoms,
+            deleteProduct:_deleteProduct
         };
         
     }]);
